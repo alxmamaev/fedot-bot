dc24828ec29c416e59faea8e110c311a7857801e
AlexanderMamaev <alxmamaev@ya.ru>
2017-03-08 22:33:37 +0500

    Add warning massnger

--- modules/warning.py
+++ modules/warning.py
@@ -1,11 +1,22 @@
 # coding: utf8
+import jinja2
 
 def init(bot):
-	pass
+	bot.handlers["warning-start"] = start
+	bot.handlers["warning-send"] = send
 
 def start(bot, message):
-	START_MESSAGE = bot.constant["warning-start"]
-	pass
+	START_MESSAGE = bot.const["get-warning"]
+	bot.telegram.send_message(message.u_id, START_MESSAGE)
 
-def send():
-	pass
+	bot.user_set(message.u_id, "next_handler", "warning-send")
+
+def send(bot, message):
+	WARNING_MESSAGE = jinja2.Template(bot.const["warning"]).render(warning = message.text)
+
+	users = bot.user_get(0, "users")
+
+	for user in users:
+		bot.telegram.send_message(user["id"], WARNING_MESSAGE, parse_mode = "Markdown")
+
+	bot.telegram.send_message(message.u_id, "Готово", parse_mode = "Markdown")
