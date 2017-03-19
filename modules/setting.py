8b0630d14f4ea2fd6d2e80934635bac77ca1f60c
AlexanderMamaev <alxmamaev@ya.ru>
2017-03-19 20:27:47 +0500

    Add login by telegram

---
+++ modules/setting.py
@@ -0,0 +1,2 @@
+def init(bot):
+	pass
---
+++ modules/weblogin.py
@@ -0,0 +1,24 @@
+import telebot
+import random
+
+STANDART_URL = "http://127.0.0.1:8080/login/" 
+def init(bot):
+	bot.handlers["web-login"] = start
+
+def generate_key(n):
+	key = ""
+	for i in range(n):
+		key += random.choice("qwertyuioopasdfghjklzxcvbnm")
+	return key
+
+def start(bot, message):
+	key = generate_key(10)
+	user_id = message.u_id
+	url = STANDART_URL+key
+
+	print(4)
+	keys = bot.user_get(0, "login_keys") or {}
+	keys[key] = user_id
+	bot.user_set(0, "login_keys", keys)
+
+	bot.telegram.send_message(message.u_id, url)
